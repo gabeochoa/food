161c61496e4807afd38b4c81cdf276b956b5c649
Gabe Ochoa <gabeochoa@meta.com>
2024-10-23 20:10:11 -0400

    format

--- ec.js
+++ ec.js
@@ -37,67 +37,66 @@ const EC = {
     "HoldsOre": [],
 };
 
-
-function randomOre(){
-    const ores = [
-        "iron",
-    ];
-    const index = Math.floor(Math.random() * ores.length);
-    return ores[index]
+function randomOre() {
+  const ores = ["iron"];
+  const index = Math.floor(Math.random() * ores.length);
+  return ores[index];
 }
 
-let NEXT_ENTITY_ID = 0
+let NEXT_ENTITY_ID = 0;
 
 class Entity {
-    constructor(x, y, components){
-        this.id = NEXT_ENTITY_ID++;
-        this.pos = createVector(x, y)
-        for(let component of components){
-            this.add_component(component)
-        }
+  constructor(x, y, components) {
+    this.id = NEXT_ENTITY_ID++;
+    this.pos = createVector(x, y);
+    for (let component of components) {
+      this.add_component(component);
     }
+  }
 
-    add_component(component){
-        // console.log(component)
-        if(!(component.name in EC)){
-            console.error("component ", component, " missing from EC", EC)
-        }
-        EC[component.name].push(this.id)
-
-        if(component.fields){
-            this.add_component_fields(component)
-        }
+  add_component(component) {
+    // console.log(component)
+    if (!(component.name in EC)) {
+      console.error("component ", component, " missing from EC", EC);
     }
+    EC[component.name].push(this.id);
 
-    add_component_fields(component){
-        for (const [key, value] of Object.entries(component.fields)) {
-            let fields = {};
-            switch(value){
-                case "Vector":
-                    fields[key] = createVector(0,0)
-                    break;
-                case "ID":
-                    fields[key] = null
-                    break;
-                case "OreType":
-                    fields[key] = randomOre();
-                    break
-                case "Amount":
-                    fields[key] = 0;
-                    break
-                default:
-                    console.warn("Missing handler for ", value)
-                    break;
-            }
-            this[component.name] = fields;
-        }
+    if (component.fields) {
+      this.add_component_fields(component);
     }
+  }
+
+  add_component_fields(component) {
+    for (const [key, value] of Object.entries(component.fields)) {
+      let fields = {};
+      switch (value) {
+        case "Vector":
+          fields[key] = createVector(0, 0);
+          break;
+        case "ID":
+          fields[key] = null;
+          break;
+        case "OreType":
+          fields[key] = randomOre();
+          break;
+        case "Amount":
+          fields[key] = 0;
+          break;
+        default:
+          console.warn("Missing handler for ", value);
+          break;
+      }
+      this[component.name] = fields;
+    }
+  }
 }
 
-
-function remove_entity(id){
-    delete entities[id];
-    for(component of Object.values(EC)){
-        delete component[id]
-    }
+function remove_entity(id) {
+  console.log("deleting ", id);
+  delete entities[id];
+  for (let component of Object.values(EC)) {
+    component = component.filter((e) => {
+      return e.id == id;
+    });
+  }
 }
--- system.js
+++ system.js
@@ -1,64 +1,76 @@
-function find_matching_ids(cmps){
-    let ids = [];
-    for(let cmp of cmps){
-        ids = ids.concat(EC[cmp.name]).unique()
-    }
-    return ids;
+function find_matching_ids(cmps) {
+  let ids = [];
+  for (let cmp of cmps) {
+    ids = ids.concat(EC[cmp.name]).unique();
+  }
+  return ids;
 }
 
-function for_components(cmps, cb){
-    let ids = find_matching_ids(cmps)
-    for(let id of ids){
-        e = entities[id];
-        if(e == null || e == undefined){
-            // console.warn("Entity ", id, " not found")
-            continue;
-        }
-        let components = cmps.map(cmp => {
-            return e[cmp.name]
-        })
-        cb(e, ...components)
+function for_components(cmps, cb) {
+  let ids = find_matching_ids(cmps);
+  for (let id of ids) {
+    e = entities[id];
+    if (e == null || e == undefined) {
+      // console.warn("Entity ", id, " not found")
+      continue;
     }
+    let components = cmps.map((cmp) => {
+      return e[cmp.name];
+    });
+    cb(e, ...components);
+  }
 }
 
-function to_ents(ids){
-    let ents = [];
-    for(let id of ids){
-        e = entities[id];
-        if(e == null || e == undefined){
-            // console.warn("Entity ", id, " not found")
-            continue;
-        }
-        ents.push(e)
-    }
-    return ents;
+function is_valid_entity(entity_id) {
+  if (entity_id == null || entity_id == undefined) return false;
+  e = entities[entity_id];
+  if (e == null || e == undefined) {
+    return false;
+  }
+  return true;
 }
 
-function find_closest_with(cmp, position, filter_fn){
-    let ids = find_matching_ids([cmp])
-    let closest = null;
-    for(let id of ids){
-        e = entities[id];
-        if(e == null || e == undefined){
-            // console.warn("Entity ", id, " not found")
-            continue;
-        }
-        if(filter_fn && !filter_fn(e)){
-            continue;
-        }
-        if(closest == null){
-            closest = e
-            continue;
-        }
-        if(distance(position, e.pos) < distance(position, closest.pos)){
-            closest = e
-            continue;
-        }
+function to_ents(ids) {
+  let ents = [];
+  for (let id of ids) {
+    e = entities[id];
+    if (e == null || e == undefined) {
+      // console.warn("Entity ", id, " not found")
+      continue;
     }
-    return closest;
+    ents.push(e);
+  }
+  return ents;
 }
 
-function count_entities_with(cmp){
-    let ids = to_ents(find_matching_ids([cmp]))
-    return ids.length;
+function find_closest_with_all(cmps, position, filter_fn) {
+  let ids = find_matching_ids(cmps);
+  let closest = null;
+  for (let id of ids) {
+    e = entities[id];
+    if (e == null || e == undefined) {
+      // console.warn("Entity ", id, " not found")
+      continue;
+    }
+    if (filter_fn && !filter_fn(e)) {
+      continue;
+    }
+    if (closest == null) {
+      closest = e;
+      continue;
+    }
+    if (distance(position, e.pos) < distance(position, closest.pos)) {
+      closest = e;
+      continue;
+    }
+  }
+  return closest;
+}
+function find_closest_with(cmp, position, filter_fn) {
+  return find_closest_with_all([cmp], position, filter_fn);
+}
+
+function count_entities_with(cmp) {
+  let ids = to_ents(find_matching_ids([cmp]));
+  return ids.length;
 }
